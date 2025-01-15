2037a9ce82c6472bdb4e43e66e0248b6c4e9eb7c
bumbu5571 <gun5571@rambler.ru>
2025-01-15 18:24:54 +0300

    style: global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
 import HistoricalEvents from "./components/HistoricalEvents/HistoricalEvents";
+import { createGlobalStyle } from 'styled-components';
 
+const GlobalStyle = createGlobalStyle`
+ *  {
+    box-sizing: border-box;
+    font-family: "PT Sans", serif;
+    }
+
+  body {
+    margin: 0;
+    padding: 0;
+  }
+`
 
 export default function App() {
   return (
     <>
+      <GlobalStyle />
       <HistoricalEvents />
     </>
   )
