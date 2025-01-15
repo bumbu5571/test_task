f25adea775e8a3520efffae9a8ddb00988da50b0
bumbu5571 <gun5571@rambler.ru>
2025-01-15 18:30:40 +0300

    style: conponent HistoricalEventsConteiner

--- src/components/HistoricalEvents/HistoricalEvents.tsx
+++ src/components/HistoricalEvents/HistoricalEvents.tsx
@@ -2,15 +2,21 @@ import EventDetailsSlider from "./EventDetailsSlider/EventDetailsSlider";
 import Line from "./Line/Line";
 import TimelineHeader from "./TimelineHeader/TimelineHeader";
 import TimePeriodSelector from "./TimePeriodSelector/TimePeriodSelector";
+import styled from "styled-components";
+
+const HistoricalEventsConteiner = styled.div`
+  width: 90em;
+  margin: 0 auto;
+`;
 
 export default function HistoricalEvents() {
   return (
-    <>
+    <HistoricalEventsConteiner>
       <TimelineHeader></TimelineHeader>
       <TimePeriodSelector></TimePeriodSelector>
       <EventDetailsSlider></EventDetailsSlider>
       <Line></Line>
       <Line></Line>
-    </>
+    </HistoricalEventsConteiner>
   )
 }
